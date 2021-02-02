d74fbf07da94fdd5bbc331fea8c2e0abcb9de866
Megan Arellano <ssciolist@gmail.com>
2021-02-02 13:03:37 -0700

    change feature test to be more reminscent of desired behavior

--- spec/features/articles/index_spec.rb
+++ spec/features/articles/index_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "articles index page", type: :feature do
         article_1 = Article.create(title: "My first post")
         article_2 = Article.create(title: "Watercolor picture")
 
-        visit "/articles"
+        visit root_path
 
         expect(page).to have_content(article_1.title)
         expect(page).to have_content(article_2.title)
