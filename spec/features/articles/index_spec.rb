233eb85abfe842d083ccda35cb0c51c6de279d40
Megan Arellano <ssciolist@gmail.com>
2021-02-16 14:01:31 -0700

    Make article titles into links

--- spec/features/articles/index_spec.rb
+++ spec/features/articles/index_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "articles index page", type: :feature do
-    it "can see all article titles and text snippet" do
+    it "can see all article titles" do
         article_1 = Article.create(title: "My first post")
         article_2 = Article.create(title: "Watercolor picture")
 
         visit root_path
 
-        expect(page).to have_content(article_1.title)
-        expect(page).to have_content(article_2.title)
+        expect(page).to have_link(article_1.title, exact: true)
+        expect(page).to have_link(article_2.title, exact: true)
     end
 end
