66cb986a8f718d3601c51ad0a4d2279e5f28c7fa
Megan Arellano <ssciolist@gmail.com>
2021-02-25 12:55:59 -0700

    Separate logging in and out from create article tests

--- spec/features/articles/create_spec.rb
+++ spec/features/articles/create_spec.rb
@@ -1,17 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe 'Create article from index', focus: :true do
+RSpec.describe 'Create article from index' do
     it 'a registered User' do
         it 'can create an article with a title' do
             sole_user = User.create(username: 'MeganArellano', password: 'MyPassword')
+            allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(sole_user)
 
-            visit '/admin'
-
-            fill_in 'user[username]', with sole_user.username
-            fill_in 'user[password]', with sole_user.password
-            click_on 'Login'
-            
-            expect(current_path).to eq(root_path)
+            visit root_path
             click_on 'Create new article'
 
             expect(current_path).to eq('/articles/new')
@@ -24,11 +19,17 @@ RSpec.describe 'Create article from index', focus: :true do
         end
     end
 
-    it 'a visitor on the root page' do
-        it 'will not see any link to create a post' do
+    it 'a visitor' do
+        it 'will not see any link to create an article on root page' do
             visit root_path
 
             expect(page).not_to have_link('Create new article')
+        end
+
+        it 'can not visit create an article directly' do
+            visit '/articles/new'
+
+            expect(page).to have_content("The page you were looking for doesn't exist")
         end
     end
 end
---
+++ spec/features/users/logout_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'Log out function exists', focus: :true do
+    it 'a registered User can log in and out' do
+            sole_user = User.create(username: 'MeganArellano', password: 'MyPassword')
+
+            visit '/admin'
+
+            fill_in 'user[username]', with sole_user.username
+            fill_in 'user[password]', with sole_user.password
+            click_on 'Login'
+            
+            expect(current_path).to eq(root_path)
+            click_on 'Log out'
+
+            expect(current_path).to eq(root_path)
+
+            expect(page).not_to have_link('Create new article')
+        end
+    end
+end
