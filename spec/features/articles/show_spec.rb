d955c87eb4080dc62b493c4c68470aa3cba495cf
Megan Arellano <ssciolist@gmail.com>
2021-07-14 13:01:43 -0600

    Visitor can't see draft articles show, registered user can

--- spec/features/articles/show_spec.rb
+++ spec/features/articles/show_spec.rb
@@ -1,26 +1,41 @@
 require 'rails_helper'
 
-RSpec.describe 'A visitor' do
-    it 'can visit published article with slug url' do
-        article_1 = Article.create(
-            title: 'My first post',
-            body: 'A long diatribe',
-            published: true
-        )
+RSpec.describe 'View article show:' do
+    context 'A vistor' do 
+        it 'can visit published article with slug url' do
+            article_1 = Article.create(
+                title: 'My first post',
+                body: 'A long diatribe',
+                published: true
+            )
 
-        visit article_path(article_1)
+            visit article_path(article_1)
 
-        slug_title = article_1.title.downcase.gsub(' ', '-')
-        expect(page).to have_current_path("/articles/#{slug_title}")
-        expect(page).to have_content(article_1.title)        
-        expect(page).to have_content(article_1.body.body.to_html)        
+            slug_title = article_1.title.downcase.gsub(' ', '-')
+            expect(page).to have_current_path("/articles/#{slug_title}")
+            expect(page).to have_content(article_1.title)        
+            expect(page).to have_content(article_1.body.body.to_html)        
+        end
+
+        it 'cannot visit an unpublished article' do
+            unpubbed_article = Article.create(title: 'Nothing yet')
+
+            visit article_path(unpubbed_article)
+
+            expect(page).to have_content("The page you were looking for doesn't exist")
+        end
     end
+    
+    context 'A registered user' do
+        include_context 'registered_user'
+        
+        it 'can visit an unpublished article' do
+            unpubbed_article = Article.create(title: 'Nothing yet', body: 'something')
 
-    it 'cannot visit an unpublished article' do
-        unpubbed_article = Article.create(title: 'Nothing yet')
+            visit article_path(unpubbed_article)
 
-        visit article_path(unpubbed_article)
-
-        expect(page).to have_content("The page you were looking for doesn't exist")
+            expect(page).to have_content(unpubbed_article.title)        
+            expect(page).to have_content(unpubbed_article.body.body.to_html)  
+        end
     end
 end
