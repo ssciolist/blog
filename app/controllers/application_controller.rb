2ddc674c8d84bdb97f4064225b40fe859f8069ec
Megan Arellano <ssciolist@gmail.com>
2021-07-14 12:59:46 -0600

    Move more helper methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
-    helper_method :current_user
+    helper_method :registered_user, :not_found_404
 
-    def current_user
-        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    def registered_user
+        @registered_user ||= User.find(session[:user_id]) if session[:user_id]
     end
+
+    def not_found_404
+        render file: "#{Rails.root}/public/404.html"
+    end
+
+    def require_registered_user
+        not_found_404 unless registered_user
+    end
+
 end
