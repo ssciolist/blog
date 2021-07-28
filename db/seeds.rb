9776c5a1b05d87682727418498669cbe45fb065c
Megan Arellano <ssciolist@gmail.com>
2021-07-28 10:45:19 -0600

    Add an author user for deployment purposes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+author = User.create(username: ENV['PRIMARY_USER'], password: ENV['PRIMARY_USER_PW'])
