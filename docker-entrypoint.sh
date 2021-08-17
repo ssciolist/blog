3237639a7a4c753b94d738085e954e6922132a3e
Megan Arellano <ssciolist@gmail.com>
2021-08-17 11:26:22 -0600

    WIP: entrypoint now has bundle instructions

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,9 @@ if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
 
+bundle config build.nokogiri --use-system-libraries
+# ^ Not sure I need this one anymore
+bundle check || bundle install --jobs=3 --retry=3
 bundle exec rails db:prepare
 
 bundle exec rails s -b 0.0.0.0
