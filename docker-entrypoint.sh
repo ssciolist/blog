b8bfa396366c39eee71f9b6b2dd6cdda8192f630
Megan Arellano <ssciolist@gmail.com>
2021-09-16 08:52:49 -0600

    Add bare minimum docker-entrypoint file

---
+++ docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+if [ -f tmp/pids/server.pid ]; then
+  rm tmp/pids/server.pid
+fi
+
+#bundle config build.nokogiri --use-system-libraries
+# ^ Not sure I need this one anymore
+bundle check || bundle install --jobs=9 --retry=3
+
+bundle exec rails db:prepare
+
+bundle exec rails s -b 0.0.0.0
