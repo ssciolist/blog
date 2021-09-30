333ba58bec49239071851d1ada996fd4e2ac294b
Megan Arellano <ssciolist@gmail.com>
2021-09-30 09:49:18 -0600

    Remove precompile step from entrypoint, remove gem install, switch to CMD exec

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,9 +5,6 @@ if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
 
-bundle check || bundle install --jobs=9 --retry=3
-bundle exec rake assets:precompile
-
 bundle exec rails db:prepare
 
-bundle exec rails s -b 0.0.0.0
+exec "$@"
